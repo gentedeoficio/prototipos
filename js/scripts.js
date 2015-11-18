9b29e06a4bb6aa52ed3268c656d6c5f396d14207
Edgar Rojas <ejrl64@hotmail.com>
2015-11-18 08:02:25 -0430

    Ajuste Página Inicio

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
 // Empty JS for your own code to be here
+// $( document ).ready(function() {
+    $("[rel='tooltip']").tooltip();
+
+    $('.thumbnail').hover(
+        function(){
+            $(this).find('.caption').slideDown(250); //.fadeIn(250)
+        },
+        function(){
+            $(this).find('.caption').slideUp(250); //.fadeOut(205)
+        }
+    );
+});
